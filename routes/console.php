bf6594bc222b22966661f6988a7496599d826f61
Rul/E <136341720+syhrlf-e@users.noreply.github.com>
2026-02-28 19:56:30 +0700

    fix(console): move use statements to top of file to fix PHP parse error breaking entire application

--- routes/console.php
+++ routes/console.php
@@ -2,13 +2,12 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Facades\DB;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
-
-use Illuminate\Support\Facades\Schedule;
-use Illuminate\Support\Facades\DB;
 
 // Bersihkan sesi expired setiap 15 menit (pengganti session:gc yang belum tersedia)
 Schedule::call(function () {
